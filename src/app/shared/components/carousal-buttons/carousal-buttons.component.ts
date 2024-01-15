1b4e89357805e88baab500ce78dc54bcc8c3656c
sohail-ansari02 <syansari02@gmail.com>
2024-01-15 17:06:08 +0530

    creaetd an compoentn for naeim

---
+++ src/app/shared/components/carousal-buttons/carousal-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carousal-buttons',
+  templateUrl: './carousal-buttons.component.html',
+  styleUrls: ['./carousal-buttons.component.scss']
+})
+export class CarousalButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
