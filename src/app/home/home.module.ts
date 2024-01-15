d14d76dfd6adec035e540abf265816cb93a23b4d
sohail-ansari02 <syansari02@gmail.com>
2024-01-15 13:11:08 +0530

    imeplemted stat sections

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -8,6 +8,7 @@ import { AboutMeComponent } from './sections/about-me/about-me.component';
 import { SharedModule } from '../shared/shared.module';
 import { InfoTabComponent } from './components/info-tab/info-tab.component';
 import { SupportsComponent } from './sections/supports/supports.component';
+import { StatsComponent } from './sections/stats/stats.component';
 
 
 @NgModule({
@@ -16,7 +17,8 @@ import { SupportsComponent } from './sections/supports/supports.component';
     HeroComponent,
     AboutMeComponent,
     InfoTabComponent,
-    SupportsComponent
+    SupportsComponent,
+    StatsComponent
   ],
   imports: [
     CommonModule,
