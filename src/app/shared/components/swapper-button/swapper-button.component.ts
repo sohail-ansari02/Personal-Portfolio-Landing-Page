20208a97932ff845f6899de6d7540439f7578c6c
sohail-ansari02 <syansari02@gmail.com>
2024-01-21 14:17:11 +0530

    added swapper buttons

---
+++ src/app/shared/components/swapper-button/swapper-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-swapper-button',
+  templateUrl: './swapper-button.component.html',
+  styleUrls: ['./swapper-button.component.scss']
+})
+export class SwapperButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
