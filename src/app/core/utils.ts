0408e504d2d03a17b51383198b9492c0ef262956
sohail-ansari02 <syansari02@gmail.com>
2024-01-25 11:55:56 +0530

    fixed issue

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,5 +1,11 @@
+const deviceWidth = {
+  small: 27 * 16,
+  medium: 48 * 16,
+  large: 90 * 16,
+};
+
 export const isMobile = (): boolean =>
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
-  );
+  ) || window.innerWidth <= deviceWidth.small;
 export const toRem = (val: number): string => val / 16 + 'rem';
