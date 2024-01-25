42e6cf8ab896461183926ed5375076614cdb820c
sohail-ansari02 <syansari02@gmail.com>
2024-01-25 13:04:47 +0530

    hided swapper

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,11 +1,11 @@
-const deviceWidth = {
-  small: 27 * 16,
-  medium: 48 * 16,
-  large: 90 * 16,
+export const Breakpoints = {
+  small: '(max-width: 27em)',
+  medium: '(max-width: 48em)',
+  large: '(max-width: 90em)',
 };
 
 export const isMobile = (): boolean =>
   /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
     navigator.userAgent
-  ) || window.innerWidth <= deviceWidth.small;
+  ) || window.matchMedia(Breakpoints.small).matches;
 export const toRem = (val: number): string => val / 16 + 'rem';
