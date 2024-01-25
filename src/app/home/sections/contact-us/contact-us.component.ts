5b100ecb2e948f0d8e4ec524752a3f47b0b19ebd
sohail-ansari02 <syansari02@gmail.com>
2024-01-25 21:11:24 +0530

    changes

--- src/app/home/sections/contact-us/contact-us.component.ts
+++ src/app/home/sections/contact-us/contact-us.component.ts
@@ -1,13 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactUsComponent implements OnInit {
-
+  url: string = environment.ContactFormUrl;
   constructor() { }
 
   ngOnInit(): void {
