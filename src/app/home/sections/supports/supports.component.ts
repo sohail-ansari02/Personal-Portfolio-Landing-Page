6b5334b890ab5b29dd186e2bcfb71873201a08f6
sohail-ansari02 <syansari02@gmail.com>
2024-08-30 17:26:31 +0530

    added data

--- src/app/home/sections/supports/supports.component.ts
+++ src/app/home/sections/supports/supports.component.ts
@@ -4,13 +4,28 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   selector: 'app-supports',
   templateUrl: './supports.component.html',
   styleUrls: ['./supports.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SupportsComponent implements OnInit {
+  techList = [
+    'Angular.png',
+    'ReactJs.svg',
+    'SCSS.svg',
+    'TypeScript.svg',
+    'JavaScript.svg',
+    'ThreeJs.svg',
+    'HTML.svg',
+    'CSS.svg',
+    'tailwind CSS.svg',
+    'BootStrap.svg',
+    'Angular Material.svg',
+    'i18n.svg',
+    'Universal.svg',
+    'FabricJs.svg',
+    'PWA.svg',
+    'Git.svg',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
