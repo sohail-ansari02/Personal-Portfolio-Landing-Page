1c67ba46c5586007d38a703400ead82e931301ba
sohail-ansari02 <syansari02@gmail.com>
2024-09-04 18:46:21 +0530

    .

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Personal-Portfolio-Landing-Page';
+  title = 'Personal-Portfolio-Landing-Page' ;
 }
